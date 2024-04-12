a1f219c2d91ff35d3357c37f585b7ccd8dae2dac
Ismaïl NGUYEN <nguyen.ismail@gmail.com>
2024-04-12 14:15:04 +0800

    init server side encryption

---
+++ utils/cypher.js
@@ -0,0 +1,17 @@
+const { encryption_private_key } = require('./config.js');
+
+const encrypt = function (content) {
+    // encrypt the given content parameter using encryption_private_key as the encryption key
+
+
+
+    // return encrypted content
+
+}
+
+const decrypt = function (content) {
+    
+}
+
+exports.encrypt = encrypt;
+exports.decrypt = decrypt;
